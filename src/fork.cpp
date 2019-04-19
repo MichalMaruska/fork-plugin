b259836e3131a555fd4341b91ab5242c3c31435d
Michal Maruska <mmaruska@gmail.com>
2019-04-19 19:48:14 +0200

    provide the dummy lock macros
    Change-Id: Ib386627fba0bf9c6904f34c050fc63889b6c7f14

--- src/fork.cpp
+++ src/fork.cpp
@@ -30,7 +30,14 @@
 #define LOCK(m)    (m)->lock=1
 #define UNLOCK(m)  (m)->lock=0
 #else
+
 #error "define locking macros!"
+// m UNUSED
+#define CHECK_LOCKED(m) while (0) {}
+#define CHECK_UNLOCKED(m) while (0) {}
+#define LOCK(m)  while (0) {}
+#define UNLOCK(m)while (0) {}
+
 #endif
 
 
