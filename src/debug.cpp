8d3b04c6751455ac41c0e049e999afaa37ec0650
Michal Maruska <mmaruska@gmail.com>
2019-04-24 06:55:23 +0200

    debug.cpp: retrieve the keysym for broken xkbi FIXME: incomplete headers
    Change-Id: I1ea967c5bddb8852c7bcbe6f0184e74d6b26f760

--- src/debug.cpp
+++ src/debug.cpp
@@ -2,6 +2,23 @@
 
 extern "C"
 {
+#include <X11/Xdefs.h>          /* for Bool */
+// #include <X11/Xproto.h>   // KeySym #define KeySym CARD32
+#include <X11/X.h>
+#include <X11/Xfuncproto.h>  // _XFUNCPROTOBEGIN
+
+// fixme: this *requires* Bool!
+
+#include <stdio.h> // FILE
+#include <xorg/xkbsrv.h>
+
+
+
+
+// #include <xorg/inputstr.h>
+
+#include <X11/keysym.h>
+
 #include "event_ops.h"
 }
 #include "debug.h"
@@ -9,25 +26,40 @@ extern "C"
 const int BufferLength = 200;
 // #define BufferLength 200
 
+static void
+retrieve_keysym(DeviceIntPtr keybd, KeyCode key, KeySym *sym, const char **keycode_name)
+{
+    if (!keybd->key)
+        return;
+
+    XkbSrvInfoPtr xkbi= keybd->key->xkbInfo;
+
+    *keycode_name = xkbi->desc->names->keys[key].name;
+    *sym= *XkbKeySymsPtr(xkbi->desc,key);
+}
+
 /* The returned string is in static space. Don't free it! */
 const char*
 describe_key(DeviceIntPtr keybd, InternalEvent *event)
 {
-    assert (event);
-
+    assert(event);
     static char buffer[BufferLength];
-    XkbSrvInfoPtr xkbi= keybd->key->xkbInfo;
     KeyCode key = detail_of(event);
-    // assert(0 <= key <= (max_key_code - min_key_code));
-    char* keycode_name = xkbi->desc->names->keys[key].name;
 
-    const KeySym *sym= XkbKeySymsPtr(xkbi->desc,key);
+    const char *keycode_name = "";
+    KeySym sym = XK_space;
+
+    retrieve_keysym(keybd, key, &sym, &keycode_name);
+    // assert(0 <= key <= (max_key_code - min_key_code));
+
+#if 0
     if ((!sym) || (! isalpha(*(unsigned char*)sym)))
         sym = (KeySym*) " ";
+#endif
 
     snprintf(buffer, BufferLength, "(%s) %d %4.4s -> %c %s (%u)",
-             keybd->name,
-             key, keycode_name,(char)*sym,
+             ((long)keybd->name<100)?"":keybd->name, key, keycode_name,
+             (char)sym,
              event_type_brief(event),(unsigned int)time_of(event));
 
     return buffer;
