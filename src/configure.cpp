4648090592812b7194c262a534e6cbf7b8427310
Michal Maruska <mmaruska@gmail.com>
2019-04-24 22:17:30 +0200

    FIX: Verify that xorg-server.h must be included BEFORE xorg/inputstr.h ???
    Change-Id: Ic5915a5782a8aeb638dbff042c65cdd3b99ece34

--- src/configure.cpp
+++ src/configure.cpp
@@ -3,6 +3,31 @@
 #include "config.h"
 #include "debug.h"
 
+// bug: this makes it crash:
+
+
+extern "C" {
+
+//#include <X11/Xmd.h>
+
+#include <xorg-server.h>
+
+#if 0
+#include <X11/X.h>
+#include <X11/Xproto.h>
+#include <X11/keysym.h>
+#endif
+// must be before:
+
+//#include <X11/X.h>
+//#include <X11/Xdefs.h>
+//#include <X11/Xproto.h>
+
+    // this is the bug!
+#include <xorg/inputstr.h>
+}
+
+// bug these 2 ^ v
 #include "configure.h"
 #include "fork.h"
 #include "history.h"
