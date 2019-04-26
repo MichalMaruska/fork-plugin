4276d88665b9a30e6269e53cfb08789711e1db8c
Michal Maruska <mmaruska@gmail.com>
2019-04-26 06:53:55 +0200

    event_ops.h includes fix: xorg-server.h must come first.
    _XSERVER64 1 is used in X11 headers! for the size of ..
    XID
    Time
    
    Change-Id: Ia4228c010cdd6bec8217b160ca1bc461bc947038
    
    # Conflicts:
    #	src/event_ops.h

--- src/event_ops.h
+++ src/event_ops.h
@@ -3,6 +3,8 @@
 #include <xorg-server.h>
 // to include the below files *correctly*, I need the the config!
 #include <X11/X.h>
+
+// #include <X11/Xproto.h>
 // specific keysyms:
 #include <X11/keysym.h>
 
