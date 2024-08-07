c987fb870b9a71b3c30b1f18ea22bd29021000a6
Michal Maruska <mmaruska@gmail.com>
2024-08-07 19:07:57 +0200

    history.h: const only in the definition. Not in declaration
    unless reference

--- src/history.h
+++ src/history.h
@@ -16,7 +16,7 @@ typedef struct {
 /* (100) */
 typedef circular_buffer<archived_event*> last_events_type;
 
-extern archived_event* make_archived_event(const key_event* const ev);
+extern archived_event* make_archived_event(const key_event *ev);
 extern int dump_last_events_to_client(PluginInstance* plugin, ClientPtr client, int n);
 
 void dump_last_events(PluginInstance* plugin);
