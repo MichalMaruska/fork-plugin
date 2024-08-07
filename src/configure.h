efcfa2e6160e98a4874a70f44db45dc6ebe0e4af
Michal Maruska <mmaruska@gmail.com>
2024-08-07 20:10:35 +0200

    configure.h drop includes

--- src/configure.h
+++ src/configure.h
@@ -2,13 +2,6 @@
 #define CONFIGURE_H
 
 extern "C" {
-#include <X11/Xmd.h>
-
-#include <xorg-server.h>
-
-#include <X11/X.h>
-#include <X11/Xproto.h>
-#include <X11/keysym.h>
 #include <xorg/inputstr.h>
 }
 
