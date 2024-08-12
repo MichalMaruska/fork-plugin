8b3f11e62e842f9edbf7f67674fa2d4e5a0665cb
Michal Maruska <mmaruska@gmail.com>
2024-08-12 10:34:39 +0200

    history: drop X headers -- although fork_requests remains

--- src/history.h
+++ src/history.h
@@ -1,13 +1,6 @@
 #ifndef _HISTORY_H_
 #define _HISTORY_H_
 
-#if 0
-extern "C" {
-#include <xorg/inputstr.h>
-#include "fork_requests.h"
-}
-#endif
-
 
 // #define DEBUG 1
 #include "circular.h"
@@ -19,19 +12,15 @@ extern "C" {
 // #include "../include/archived_event.h"
 
 typedef struct {
-  // InternalEvent*
   PlatformEvent*  p_event;
   KeyCode forked; /* if forked to (another keycode), this is the original key */
 } key_event;
-
-
 
 /* (100) */
 typedef circular_buffer<archived_event*> last_events_type;
 
 extern archived_event* make_archived_event(const key_event *ev);
 extern int dump_last_events_to_client(PluginInstance* plugin, ClientPtr client, int n);
-
 void dump_last_events(PluginInstance* plugin);
 
 #endif
