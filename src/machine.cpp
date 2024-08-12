74b97763e39cfbeec576185cdd55aaf3ed0c24f8
Michal Maruska <mmaruska@gmail.com>
2024-08-12 10:19:28 +0200

    make sure the relevant templated functions are generated
    + all invoked from them.
    
    Those are then used from other .cpp files

--- src/machine.cpp
+++ src/machine.cpp
@@ -907,3 +907,9 @@ forkingMachine<Keycode, Time>::step_fork_automaton_by_key(key_event *ev)
             mdb("----------unexpected state---------\n");
     }
 }
+
+// explicit instantation:
+template void forkingMachine<Time, KeyCode>::accept_event(PlatformEvent* pevent);
+template void forkingMachine<Time, KeyCode>::switch_config(int);
+template void forkingMachine<Time, KeyCode>::step_in_time_locked(unsigned char);
+
