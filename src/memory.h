363dfb0fbe3d2ce0d690ab83219f52988c1c1e5a
Michal Maruska <mmaruska@gmail.com>
2024-09-25 00:55:57 +0200

    memory correct header

--- src/memory.h
+++ src/memory.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdlib>
 // I want to track the memory usage, and warn when it's too high.
 extern size_t memory_balance;
 
@@ -8,18 +9,17 @@ extern size_t memory_balance;
 inline
 void* mmalloc(size_t size)
 {
-  void* p = malloc(size);
-  if (p)
-    {
-      memory_balance += size;
-    // disable till template
+    void* p = malloc(size);
+    if (p) {
+        memory_balance += size;
+        // disable till template
 #if 0
-      if (memory_balance > sizeof(machineRec) + sizeof(PluginInstance) + 2000)
-        ErrorF("%s: memory_balance = %ld\n", __FUNCTION__, memory_balance);
+        if (memory_balance > sizeof(machineRec) + sizeof(PluginInstance) + 2000)
+            ErrorF("%s: memory_balance = %ld\n", __FUNCTION__, memory_balance);
 #endif
 
     }
-  return p;
+    return p;
 }
 
 // #pragma message ( "Debug configuration - OK" )
