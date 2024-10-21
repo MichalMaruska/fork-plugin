80cfd8305372ab2c0390658fec84b986e528d7fc
Michal Maruska <mmaruska@gmail.com>
2024-10-21 17:25:41 +0200

    xmachine: provide the archived_event type

--- src/xmachine.h
+++ src/xmachine.h
@@ -7,13 +7,19 @@ extern "C"
     // We are inside server:
 #include <xorg-server.h>
 
+// todo: archived_event coud be avoided ... it's X specific
+// plugin should know how to translate/send them away.l
+#include "fork_requests.h"
+#undef max
+#undef min
+
     // the types:
 #include <X11/X.h>
 #undef max
 #undef min
 }
 
-using machineRec = forkingMachine<KeyCode, Time>;
+using machineRec = forkingMachine<KeyCode, Time, archived_event>;
 
 // fixme:
 #define plugin_machine(plugin) ((machineRec*)(plugin->data))
