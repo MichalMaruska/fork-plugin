cc950e34784aac4187ae57abe344862a80383a6b
Michal Maruska <mmaruska@gmail.com>
2024-11-14 14:52:47 +0100

    improve the time comparison to stay in unsigned

--- src/machine.h
+++ src/machine.h
@@ -20,12 +20,13 @@ namespace forkNS {
 
 template <typename Time>
 bool time_difference_more(Time now, Time past, Time limit_difference) {
-    return ( (now - past) > limit_difference);
+    return (now > past + limit_difference);
+    // return ( (now - past) > limit_difference);
 }
 
 
 /* fixme: inherit from xorg! */
-constexpr int MAX_KEYCODE=256;
+constexpr int MAX_KEYCODE = 256;
 
 /**
  * machine keeps log of `archived_event_t'
