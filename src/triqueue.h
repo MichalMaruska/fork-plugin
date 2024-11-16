5cc5fa2e378342a199fd499867c47c2671a1e739
Michal Maruska <mmaruska@gmail.com>
2024-11-16 12:38:49 +0100

    log around ops

--- src/triqueue.h
+++ src/triqueue.h
@@ -71,13 +71,21 @@ public:
     }
 
     void rewind_middle() {
+        log_queues(__func__);
         reverse_splice(internal_queue, input_queue);
+        log_queues("post");
     }
-
 
     // modifiers:
     void push(const item_t &i) {
+        env->log("%s: %lu\n", __func__, sizeof(i));
+
+        dump_item(i);
         input_queue.push(i);
+
+        env->log("double check\n");
+        dump_item(input_queue.front());
+        log_queues(__func__);
     }
 
     bool can_pop() {
@@ -85,7 +93,10 @@ public:
     }
 
     item_t pop() {
-        return output_queue.pop();
+        log_queues(__func__);
+        item_t item = output_queue.pop();
+        dump_item(item);
+        return item;
     }
 
     const item_t& peek_third() {
@@ -105,7 +116,9 @@ public:
     // push();
     // tq.shift_to_first();
     void move_to_first() {
+        log_queues(__func__);
         output_queue.push(internal_queue.pop());
+        log_queues(" post");
     }
     void move_to_second() {
         internal_queue.push(input_queue.pop());
