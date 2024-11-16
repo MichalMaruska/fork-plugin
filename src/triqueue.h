6bc36f8d142036e2d31419e1bbb274b3a3498c74
Michal Maruska <mmaruska@gmail.com>
2024-11-16 23:25:42 +0100

    use circular buffer instead of the 3 queues

--- src/triqueue.h
+++ src/triqueue.h
@@ -1,38 +1,42 @@
 #pragma once
 
-#include "queue.h"
+#include "circular.h"
+#include <memory>
 
 template <typename item_t, typename Environment_t>
 class triqueue_t {
-    // interface:
-    typedef forkNS::my_queue<item_t> list_with_tail;
+
+    using circular_buffer_t = circular_buffer<item_t, false, std::allocator<item_t>>;
+    using iterator = typename circular_buffer_t::iterator;
 
 public:
     inline static const Environment_t *env = nullptr;
 private:
-    list_with_tail internal_queue;
-    /* Still undecided events: these events alone don't decide
-       how to interpret the first event on the queue.*/
-    list_with_tail input_queue; /* Not yet processed at all. Since we wait for external
-                  * events to resume processing (Grab is active-frozen) */
-    list_with_tail output_queue; /* We have decided, so possibly modified events (forked),
-                   * Externals don't accept, so we keep them. */
 
-// internal_queue
-// empty
+    circular_buffer_t buffer;
+    iterator end_output;
+    iterator end_internal;
 
-
-// output_queue.push
-//
-
-// input_queue
-//    length  empty
-//  pop push
 
     void log_queues(const char* msg) {
-        env->log("%s: %lu %lu %lu\n", msg, output_queue.size(),
-                 internal_queue.size(), input_queue.size());
-    }
+        if (env == nullptr)
+            return;
+#if 0
+        env->log("(%s) %s: %lu\n", __func__, msg, buffer.size());
+
+
+        env->log("%s: %lu %lu %lu %lu\n", msg,
+                 buffer.begin().pos_,
+                 end_output.pos_,
+                 end_internal.pos_,
+                 buffer.end().pos_);
+#endif
+        env->log("%s(%s): %lu %lu %lu\n", msg, __func__,
+                 end_output - buffer.begin(),
+                 end_internal - end_output,
+                 buffer.end() - end_internal);
+    };
+
     void dump_item(const item_t &item) {
 #if 0
         constexpr int per_line = 50;
@@ -50,104 +54,99 @@ private:
     }
 
 public:
-    triqueue_t(int capacity) : internal_queue("internal"),
-                               input_queue("input_queue"),
-                               output_queue("output_queue")
-        {};
+        triqueue_t(int capacity) : buffer(circular_buffer_t(capacity)),
+                                   end_output(buffer.begin()),
+                                   end_internal(buffer.begin())
+        {
+            log_queues(__func__);
+        };
 
     // Queries
     // int length();
 
     bool empty() {
-        return (output_queue.empty() && input_queue.empty() && internal_queue.empty());
+        return (buffer.empty());
     }
 
     bool middle_empty() {
-        return (input_queue.empty() && internal_queue.empty());
+        return (end_internal == end_output);
     }
 
     bool third_empty() {
-        return input_queue.empty();
+        return end_internal == buffer.end();
     }
 
     void rewind_middle() {
         log_queues(__func__);
-        reverse_splice(internal_queue, input_queue);
+        end_internal = end_output;
         log_queues("post");
     }
 
     // modifiers:
-    void push(const item_t &i) {
-        env->log("%s: %lu\n", __func__, sizeof(i));
+    void push(const item_t &item) {
+#if 0
+        env->log("%s: %lu\n", __func__, sizeof(item));
+        dump_item(item);
+#endif
+        buffer.push_back(item);
 
-        dump_item(i);
-        input_queue.push(i);
-
-        env->log("double check\n");
-        dump_item(input_queue.front());
-        log_queues(__func__);
+        // dump_item(input_queue.front());
+        log_queues("post-push");
+#if 0
+        peek_third();
+#endif
     }
 
     bool can_pop() {
-        return ! output_queue.empty();
+        return buffer.begin() != end_output;
     }
 
     item_t pop() {
         log_queues(__func__);
-        item_t item = output_queue.pop();
+
+        item_t& item = buffer.front();
+
+        buffer.pop_front();
+        end_internal-=1;
+        end_output-=1;
+
         dump_item(item);
+        log_queues("post");
         return item;
     }
 
     const item_t& peek_third() {
-        // fixme: not pop. peek
-        return input_queue.front();
+        const item_t& tmp = *(end_internal); // +1
+        // env->log("%s: %p\n", __func__, &tmp);
+        dump_item(tmp);
+        return tmp;
     }
-    // assert(!internal_queue.empty());
-
 
     // rewritable!
     item_t& rewrite_head() {
         // fixme: not pop. peek
-        return internal_queue.front();
+        item_t& tmp = *(end_output); // +1
+        env->log("%s: %p\n", __func__, &tmp);
+        return tmp;
     }
 
-    // insert
-    // push();
-    // tq.shift_to_first();
     void move_to_first() {
         log_queues(__func__);
-        output_queue.push(internal_queue.pop());
+        end_output++;
         log_queues(" post");
     }
+
     void move_to_second() {
         log_queues(__func__);
-
-        env->log("double check\n");
-#if 0
-        const item_t& item = input_queue.pop();
-        dump_item(item);
-        internal_queue.push(item);
-#else
-        dump_item(input_queue.front());
-        internal_queue.push(input_queue.pop());
-#endif
-        env->log("double check\n");
-        dump_item(internal_queue.front());
-
+        end_internal++;
         log_queues(" post");
     }
 
-    // internal_queue, input_queue
     const item_t* first() {
-        if (!output_queue.empty()) {
-            return &output_queue.front();
-        } else if (!internal_queue.empty()) {
-            return &internal_queue.front();
-        } else if (!input_queue.empty()) {
-            return &input_queue.front();
-        } else {
+        if (buffer.empty()) {
             return nullptr;
+        } else {
+            return &(buffer.front());
         }
     }
 };
